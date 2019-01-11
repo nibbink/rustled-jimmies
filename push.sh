d91fef96306beec0b817ee5164734d9dbe71d956
Juan <juan@villela.co>
2019-01-10 21:28:43 -0500

    Attempting Travis git push script

---
+++ push.sh
@@ -0,0 +1,22 @@
+
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout
+  git add --all
+  git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin https://23b76aa2e55b90f8e685e73b206bc7a38fc528fc@github.com/fourjuaneight/rustled-jimmies.git > /dev/null 2>&1
+  git push --quiet --set-upstream
+}
+
+setup_git
+commit_website_files
+upload_files
